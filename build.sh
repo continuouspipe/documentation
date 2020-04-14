49403fe5bc5f23583fdd0c36668023a8d3bfce7a
Kieren Evans <kevans@inviqa.com>
2020-04-14 23:57:34 +0300

    Add build.sh to render to docs/ folder for github pages

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+docker-compose -f docker-compose.yml build --pull
+docker-compose -f docker-compose.yml up -d
+rm -rf docs/ || true
+docker cp documentation_web_1:/usr/share/nginx/html/ docs/
