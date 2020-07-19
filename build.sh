5532fa5ab964ec1918aa073280475139614a3cea
Kieren Evans <kevans@inviqa.com>
2020-07-19 20:01:12 +0300

    Fix build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 docker-compose -f docker-compose.yml build --pull
 docker-compose -f docker-compose.yml up -d
 rm -rf docs/ || true
-docker cp documentation_web_1:/usr/share/nginx/html/ docs/
+docker cp documentation_web_1:/usr/share/nginx/html/docs/ docs/
